c5d1745303cff0294e28a8787fe8ebc83e18989c
Francisco Menendez Moya <menendezmoya94@gmail.com>
2016-10-04 12:23:26 +0200

    First commit, Project base.

---
+++ src/main/java/es/unizar/es/foodnet/FoodNetApplication.java
@@ -0,0 +1,12 @@
+package es.unizar.es.foodnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodNetApplication.class, args);
+	}
+}
---
+++ src/test/java/es/unizar/es/foodnet/FoodNetApplicationTests.java
@@ -0,0 +1,16 @@
+package es.unizar.es.foodnet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodNetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
