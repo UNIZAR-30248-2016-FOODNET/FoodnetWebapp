3bfa28237cf8e895b44f7116c1da1c3814c57ad4
Francisco Menendez Moya <menendezmoya94@gmail.com>
2016-10-23 18:09:50 +0200

    Inicializacion mongo contextual

--- src/main/java/es/unizar/es/foodnet/FoodNetApplication.java
+++ src/main/java/es/unizar/es/foodnet/FoodNetApplication.java
@@ -12,22 +12,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class FoodNetApplication implements CommandLineRunner {
 
+	private final RepositorioProducto repositorioProducto;
+	private final RepositorioCategoria repositorioCategoria;
+	private final RepositorioSupermercado repositorioSupermercado;
+
 	@Autowired
-	private RepositorioProducto repositorioProducto;
-	@Autowired
-	private RepositorioCategoria repositorioCategoria;
-	@Autowired
-	private RepositorioSupermercado repositorioSupermercado;
+	public FoodNetApplication(RepositorioCategoria repositorioCategoria, RepositorioSupermercado repositorioSupermercado, RepositorioProducto repositorioProducto) {
+		this.repositorioCategoria = repositorioCategoria;
+		this.repositorioSupermercado = repositorioSupermercado;
+		this.repositorioProducto = repositorioProducto;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodNetApplication.class, args);
 	}
 
 	/**
-	 * Todo lo que se incluya dentro de este metodo se ejecuta al lanzar la
+	 * Lo que se incluya dentro de este metodo se ejecuta al lanzar la
 	 * aplicacion de Spring
      */
 	@Override
@@ -47,24 +53,46 @@ public class FoodNetApplication implements CommandLineRunner {
 		Supermercado mercadona = repositorioSupermercado.findByNombre("Mercadona");
 
 		// Store 5 products
-		repositorioProducto.save(new Producto(lacteo, mercadona, "Yogurt", 0.20, "http://placehold.it/650x450"));
-		repositorioProducto.save(new Producto(lacteo, mercadona, "Yogurt2", 3.80, "http://placehold.it/650x450"));
-		repositorioProducto.save(new Producto(lacteo, mercadona, "Yogurt3", 1.2, "http://placehold.it/650x450"));
-		repositorioProducto.save(new Producto(lacteo, mercadona, "Yogurt4", 0.20, "http://placehold.it/650x450"));
-		repositorioProducto.save(new Producto(lacteo, mercadona, "Yogurt5", 0.70, "http://placehold.it/650x450"));
+		Producto p1 = new Producto(lacteo, mercadona, "Yogurt", 0.20, "http://placehold.it/650x450&text=Yogurt");
+		Producto p2 = new Producto(lacteo, mercadona, "Yogurt2", 3.80, "http://placehold.it/650x450&text=Yogurt2");
+		Producto p3 = new Producto(lacteo, mercadona, "Yogurt3", 1.2, "http://placehold.it/650x450&text=Yogurt3");
+		Producto p4 = new Producto(lacteo, mercadona, "Yogurt4", 0.20, "http://placehold.it/650x450&text=Yogurt4");
+		Producto p5 = new Producto(lacteo, mercadona, "Yogurt5", 0.70, "http://placehold.it/650x450&text=Yogurt5");
 
-		// fetch all customers
-		System.out.println("Products found with findAll():");
-		System.out.println("------------------------------");
-		repositorioProducto.findAll().forEach(System.out::println);
-		System.out.println();
+		ArrayList<Producto> productosPrevios = new ArrayList<>();
+		productosPrevios.add(p1);
+		productosPrevios.add(p2);
+		productosPrevios.add(p3);
+		productosPrevios.add(p4);
+		productosPrevios.add(p5);
 
+		repositorioProducto.save(p1);
+		repositorioProducto.save(p2);
+		repositorioProducto.save(p3);
+		repositorioProducto.save(p4);
+		repositorioProducto.save(p5);
+
+		//Comprobar que se han introducido correctamente los 5 productos
+		ArrayList<Producto> productosObtenidos = (ArrayList<Producto>) repositorioProducto.findAll();
+		for(int i = 0; i < productosObtenidos.size(); i++){
+			if(!((productosObtenidos.get(i).getNombre().equals(productosPrevios.get(i).getNombre())) &&
+					(!productosObtenidos.get(i).getSupermercado().equals(productosPrevios.get(i).getSupermercado())))){
+				System.exit(1);
+			}
+		}
+
+		System.out.println("===========================================");
+		System.out.println("INICIALIZACION MONGODB COMPLETADA CON EXITO");
+		System.out.println("===========================================");
+
+		//TODO: Cuando toque, borrar o descomentar
+		/*
 		// fetch an individual customer
 		System.out.println("Producto found with findByName('Yogurt'):");
 		System.out.println("---------------------------------------");
 		System.out.println(repositorioProducto.findByNombre("Yogurt"));
 		System.out.println();
-		/*System.out.println("Customers found with findBySupermarket('Mercadona'):");
+		System.out.println("Customers found with findBySupermarket('Mercadona'):");
 		System.out.println("----------------------------------------------");
 		productRepository.findBySupermarket("Mercadona").forEach(System.out::println);
 		System.out.println();
