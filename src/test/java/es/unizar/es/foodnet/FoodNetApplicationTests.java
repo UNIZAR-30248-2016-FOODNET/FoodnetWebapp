4c3d72399156f4cfa89ae7182361de9a2b654bd7
Mario <mario94almunia@gmail.com>
2016-10-25 09:34:16 +0200

    Añadidos más test de usuario

--- src/test/java/es/unizar/es/foodnet/FoodNetApplicationTests.java
+++ src/test/java/es/unizar/es/foodnet/FoodNetApplicationTests.java
@@ -10,13 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.ExtendedModelMap;
-
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +31,8 @@ public class FoodNetApplicationTests {
 	private RepositorioUsuario repositorioUsuario;
 	@Autowired
 	private ControladorUsuario cu;
+	@Autowired
+	WebApplicationContext context;
 	private Usuario user;
 
 	@After
@@ -53,8 +60,15 @@ public class FoodNetApplicationTests {
 		cu.registrarUsuario(user);
 		Usuario usuario = repositorioUsuario.findByEmail("pepe@gmail.com");
 		assertNotNull(usuario);
-		assertNull(repositorioUsuario.findByEmail("noExisto@gmail.com"));
+	}
 
+	/**
+	 * Test para comprobar que al buscar un usuario que no existe no devuelve
+	 * ningun usuario
+	 */
+	@Test
+	public void buscarUsuarioInexistente() {
+		assertNull(repositorioUsuario.findByEmail("noExisto@gmail.com"));
 	}
 
 	/**
@@ -80,6 +94,41 @@ public class FoodNetApplicationTests {
 		assertEquals("redirect:/panelLogin",cu.autenticarUsuario("noExisto@gmail.com","daIgualPass",new MockHttpServletRequest(),new ExtendedModelMap()));
 	}
 
+	/**
+	 * Test para comprobar que al introducir un email que todavia no existe en el
+	 * registro no se produce un error y indicando "no encontrado"
+	 */
+	@Test
+	public void comprobarEmailNoDuplicado() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		mockMvc.perform(post("/comprobarEmail").param("email", "noExiste@gmail.com"))
+				.andExpect(status().isOk());
+
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		cu.comprobarEmailRegistro("noExiste@gmail.com", response);
+		assertEquals("noencontrado", response.getContentAsString().trim());
+	}
+
+	/**
+	 * Test para comprobar que al introducir un email que existe en el registro
+	 * se produce un error indicando "encontrado"
+	 */
+	@Test
+	public void comprobarEmailDuplicado() throws Exception {
+
+		user = new Usuario("pepe", "Sanchez", "pepe@gmail.com", "Zaragoza-1", "zaragoza");
+		cu.registrarUsuario(user);
+
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		mockMvc.perform(post("/comprobarEmail").param("email", user.getEmail()))
+				.andExpect(status().isOk());
+
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		cu.comprobarEmailRegistro(user.getEmail(), response);
+		assertEquals("encontrado", response.getContentAsString().trim());
+
+
+	}
 
 	@Test
 	public void contextLoads() {
