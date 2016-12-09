a95c568804469e62a98a384b9fe77b5b71378a47
Mario Navarro Claras <mario94almunia@gmail.com>
2016-12-09 12:27:44 +0100

    Test y modelo de datos historial pedido
    * Pruebas manuales sobre botones +/- y bugfix javascript
    
    * Eliminado codigo al ejecutar Spring (bugfix pruebas)
    
    * Modelo de datos y tests de historial pedidos

--- src/test/java/es/unizar/es/foodnet/model/PedidoTest.java
+++ src/test/java/es/unizar/es/foodnet/model/PedidoTest.java
@@ -5,10 +5,11 @@ import es.unizar.es.foodnet.controller.ControladorPedido;
 import es.unizar.es.foodnet.controller.ControladorUsuario;
 import es.unizar.es.foodnet.model.entity.Pedido;
 import es.unizar.es.foodnet.model.entity.Usuario;
-import es.unizar.es.foodnet.model.repository.RepositorioPedido;
 import es.unizar.es.foodnet.model.repository.RepositorioProducto;
 import es.unizar.es.foodnet.model.repository.RepositorioUsuario;
 import es.unizar.es.foodnet.model.service.ProductoCarro;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -17,51 +18,58 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.ui.ExtendedModelMap;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PedidoTest {
 
-    @Autowired
-    RepositorioPedido repositorioPedido;
-    @Autowired
-    RepositorioProducto repositorioProducto;
-    @Autowired
-    RepositorioUsuario repositorioUsuario;
-    @Autowired
-    ControladorPedido cp;
-    @Autowired
-    ControladorCarro cc;
-    @Autowired
-    ControladorUsuario cu;
-    @Autowired
-    WebApplicationContext context;
+    @Autowired RepositorioProducto repositorioProducto;
+    @Autowired RepositorioUsuario repositorioUsuario;
+    @Autowired ControladorPedido cp;
+    @Autowired ControladorCarro cc;
+    @Autowired ControladorUsuario cu;
+
+    private Usuario user;
+    private ArrayList<ProductoCarro> carroProductos;
+
+    @Before
+    public void beforeTestClass() {
+        user = repositorioUsuario.findByEmail("testPedidos@gmail.com");
+        if (user == null) {
+            user = new Usuario("test", "pedidos", "testPedidos@gmail.com", "pedidos", "zaragoza");
+            cu.registrarUsuario(user, Mockito.mock(RedirectAttributes.class));
+        }
+        carroProductos = new ArrayList<>();
+        carroProductos.add(new ProductoCarro(repositorioProducto.findByNombre("Yogurt"), 1));
+    }
+
+    @After
+    public void afterTestClass() {
+        if(user != null){
+            repositorioUsuario.delete(user);
+            user = null;
+        }
+    }
 
     /**
      * Test que comprueba que un nuevo usuario no debe devolver ningún pedido.
      */
     @Test
     public void noExistenPedidos () {
-        Usuario user = repositorioUsuario.findByEmail("testPedidos@gmail.com");
-        if (user == null) {
-            user = new Usuario("test", "pedidos", "testPedidos@gmail.com", "pedidos", "zaragoza");
-            cu.registrarUsuario(user, Mockito.mock(RedirectAttributes.class));
-        }
-
         MockHttpServletRequest request = new MockHttpServletRequest();
         ExtendedModelMap model = new ExtendedModelMap();
         request.getSession().setAttribute("user", user);
         cp.cargarPedidos(model, request);
         assertEquals(0, ((List<Pedido>) model.get("historial")).size());
-
-        repositorioUsuario.delete(user);
     }
 
     /**
@@ -69,15 +77,6 @@ public class PedidoTest {
      */
     @Test
     public void addPedido () {
-        Usuario user = repositorioUsuario.findByEmail("testPedidos2@gmail.com");
-        if (user == null) {
-            user = new Usuario("test2", "pedidos", "testPedidos2@gmail.com", "pedidos", "zaragoza");
-            cu.registrarUsuario(user, Mockito.mock(RedirectAttributes.class));
-        }
-
-        ArrayList<ProductoCarro> carroProductos = new ArrayList<>();
-        carroProductos.add(new ProductoCarro(repositorioProducto.findByNombre("Yogurt"), 1));
-
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.getSession().setAttribute("user", user);
         request.getSession().setAttribute("carroProductos", carroProductos);
@@ -86,8 +85,6 @@ public class PedidoTest {
         ExtendedModelMap model = new ExtendedModelMap();
         cp.cargarPedidos(model, request);
         assertEquals("Yogurt",((List<Pedido>) model.get("historial")).get(0).getProductos().get(0).getProducto().getNombre());
-
-        repositorioUsuario.delete(user);
     }
 
     /**
@@ -96,19 +93,37 @@ public class PedidoTest {
      */
     @Test
     public void cargarPedidosTest () {
-        Usuario user = repositorioUsuario.findByEmail("pablo@gmail.com");
         MockHttpServletRequest request = new MockHttpServletRequest();
         ExtendedModelMap model = new ExtendedModelMap();
         request.getSession().setAttribute("user", user);
         cp.cargarPedidos(model, request);
         int pedidosInicial = ((List<Pedido>) model.get("historial")).size();
 
-        ArrayList<ProductoCarro> carroProductos = new ArrayList<>();
-        carroProductos.add(new ProductoCarro(repositorioProducto.findByNombre("Yogurt"), 1));
         request.getSession().setAttribute("carroProductos", carroProductos);
         cc.finCompra(request, Mockito.mock(RedirectAttributes.class));
 
         cp.cargarPedidos(model, request);
         assertEquals(pedidosInicial + 1, ((List<Pedido>) model.get("historial")).size());
+    }
+
+    /**
+     * Crea un pedido y comprueba que el estado inicial es realizado.
+     */
+    @Test
+    public void comprobarEstadoPedidoCreado() {
+        Pedido pedido = new Pedido(user, Calendar.getInstance().getTime(), carroProductos);
+        assertEquals(pedido.getEstado(), Pedido.PEDIDO_REALIZADO);
+    }
+
+    /**
+     * Crea un pedido y comprueba que al cancelarlo su estado pasa a estar cancelado.
+     * Comprueba que al cancelar un pedido ya cancelado no hace nada y devuelve false.
+     */
+    @Test
+    public void cancelarPedido() {
+        Pedido pedido = new Pedido(user, Calendar.getInstance().getTime(), carroProductos);
+        assertTrue(pedido.cancelarPedido());
+        assertEquals(pedido.getEstado(), Pedido.PEDIDO_CANCELADO);
+        assertFalse(pedido.cancelarPedido());
     }
 }
