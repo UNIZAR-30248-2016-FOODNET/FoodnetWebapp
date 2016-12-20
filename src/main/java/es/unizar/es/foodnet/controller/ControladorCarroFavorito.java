d3a9ef64fe15d3d86e0fe0c222c78ad532b973ea
strummerTFIU <679609@unizar.es>
2016-12-20 19:44:22 +0100

    GUI + Javascript de Añadir carro favorito
    * Pruebas backend historial y bug fixes.
    
    * Modificadas pruebas para ser ejecutadas en otro Schema.
    
    * Añadido after para permitir segunda iteración de pruebas.
    
    * Añadido after para permitir segunda iteración de pruebas (2).
    
    * Modelo de datos, API y backend añadir carro favorito.
    
    * GUI + Javascript de Añadir carro favorito

--- src/main/java/es/unizar/es/foodnet/controller/ControladorCarroFavorito.java
+++ src/main/java/es/unizar/es/foodnet/controller/ControladorCarroFavorito.java
@@ -3,16 +3,18 @@ package es.unizar.es.foodnet.controller;
 import es.unizar.es.foodnet.model.entity.CarroFavorito;
 import es.unizar.es.foodnet.model.entity.Usuario;
 import es.unizar.es.foodnet.model.repository.RepositorioCarroFavorito;
-import es.unizar.es.foodnet.model.service.MessageHelper;
 import es.unizar.es.foodnet.model.service.ProductoCarro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,28 +35,33 @@ public class ControladorCarroFavorito {
     /**
      * Almacena el carro en el repositorio de carros favoritos.
      *
-     * @param request request objeto request del usuario
-     * @param ra apoyo para el feedback
-     * @return pagina del carro
+     * @param nombreFavorito cadena de caracteres con el nombre deseado para el carro
+     * @param request objeto request del usuario
+     * @param response objeto response del usuario
      */
     @RequestMapping(value="/addFavorito", method = RequestMethod.POST)
-    public String anadirCarroFavorito(HttpServletRequest request, RedirectAttributes ra){
+    public void anadirCarroFavorito(@RequestParam("name") String nombreFavorito,
+                                    HttpServletRequest request, HttpServletResponse response){
         System.out.println("Me ha llegado peticion para añadir un carro favorito");
 
         ArrayList<ProductoCarro> carroSesion = (ArrayList<ProductoCarro>) request.getSession().getAttribute("carroProductos");
         Usuario usuario = (Usuario) request.getSession().getAttribute("user");
-        String nombre = (String) request.getSession().getAttribute("nombreFavorito");
 
-        if (repository.findByNombre(nombre) == null) {
-            repository.save(new CarroFavorito(nombre, usuario, carroSesion));
-            System.out.println("Carro favorito añadido");
-            MessageHelper.addSuccessAttribute(ra,"exito.favorito","");
-        } else {
-            System.err.println("Carro favorito no añadido: ya existe un carro con ese nombre.");
-            MessageHelper.addErrorAttribute(ra,"fallo.favorito", "");
+        try{
+            PrintWriter out;
+            out = response.getWriter();
+
+            if (repository.findByNombre(nombreFavorito) == null) {
+                repository.save(new CarroFavorito(nombreFavorito, usuario, carroSesion));
+                System.out.println("Carro favorito añadido");
+                out.println("ok");
+            } else {
+                System.err.println("Carro favorito no añadido: ya existe un carro con ese nombre.");
+                out.println("error");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return "redirect:/carro";
     }
 
 
