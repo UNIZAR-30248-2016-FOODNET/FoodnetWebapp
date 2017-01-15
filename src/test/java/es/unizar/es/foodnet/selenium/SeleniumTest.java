39be779ee501b44702dcb67551f33e487090425e
Pablo <679609@unizar.es>
2017-01-15 13:11:08 +0100

    Testing chromedriver

---
+++ src/test/java/es/unizar/es/foodnet/selenium/SeleniumTest.java
@@ -0,0 +1,31 @@
+package es.unizar.es.foodnet.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class SeleniumTest {
+
+    private WebDriver browser;
+
+    @Before
+    public void setup() {
+        browser = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        browser.close();
+    }
+
+    @Test
+    public void startTest() throws Exception {
+        browser.get("http://foodnetunizar.herokuapp.com/");
+        Thread.sleep(10000);
+    }
+}
